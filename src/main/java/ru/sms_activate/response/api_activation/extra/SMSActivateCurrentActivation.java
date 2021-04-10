7a2f01fa2e0aa762b1d489a9e7087c599de8362a
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2021-04-10 21:14:32 +0300

    add SerializedName with alias in SMSActivateCurrentActivation.java to field phoneNumber

--- src/main/java/ru/sms_activate/response/api_activation/extra/SMSActivateCurrentActivation.java
+++ src/main/java/ru/sms_activate/response/api_activation/extra/SMSActivateCurrentActivation.java
@@ -1,90 +1,94 @@
 package ru.sms_activate.response.api_activation.extra;
 
+import com.google.gson.annotations.SerializedName;
 import org.jetbrains.annotations.NotNull;
 
 public class SMSActivateCurrentActivation {
-    /**
-     * Activation id.
-     */
-    private int id;
+  /**
+   * Activation id.
+   */
+  private int id;
 
-    /**
-     * Activation is forward.
-     */
-    private int forward;
+  /**
+   * Activation is forward.
+   */
+  private int forward;
 
-    /**
-     * Activation phone number.
-     */
-    private long phoneNumber;
+  /**
+   * Activation phone number.
+   */
+  @SerializedName(value = "phone", alternate = {
+    "phoneNumber", "number"
+  })
+  private long phoneNumber;
 
-    /**
-     * Service for activation.
-     */
-    private String service;
+  /**
+   * Service for activation.
+   */
+  private String service;
 
-    /**
-     * Country activation.
-     */
-    private int country;
+  /**
+   * Country activation.
+   */
+  private int country;
 
-    private SMSActivateCurrentActivation() {
-    }
+  private SMSActivateCurrentActivation() {
+  }
 
-    /**
-     * Returns the service activation.
-     *
-     * @return service activation
-     */
-    @NotNull
-    public String getService() {
-        return service;
-    }
+  /**
+   * Returns the service activation.
+   *
+   * @return service activation
+   */
+  @NotNull
+  public String getService() {
+    return service;
+  }
 
-    /**
-     * Returns the country activation.
-     *
-     * @return country activation.
-     */
-    public int getCountry() {
-        return country;
-    }
+  /**
+   * Returns the country activation.
+   *
+   * @return country activation.
+   */
+  public int getCountry() {
+    return country;
+  }
 
-    /**
-     * Returns the activation id.
-     *
-     * @return activation id
-     */
-    public int getId() {
-        return id;
-    }
+  /**
+   * Returns the activation id.
+   *
+   * @return activation id
+   */
+  public int getId() {
+    return id;
+  }
 
-    /**
-     * Returns the forward activation.
-     *
-     * @return forward activation
-     */
-    public boolean isForward() {
-        return forward == 1;
-    }
+  /**
+   * Returns the forward activation.
+   *
+   * @return forward activation
+   */
+  public boolean isForward() {
+    return forward == 1;
+  }
 
-    /**
-     * Returns the activation phone number.
-     *
-     * @return activation phone number
-     */
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
+  /**
+   * Returns the activation phone number.
+   *
+   * @return activation phone number
+   */
+  public long getPhoneNumber() {
+    return phoneNumber;
+  }
 
-    @Override
-    public String toString() {
-        return "SMSActivateCurrentActivation{" +
-          "id=" + id +
-          ", forward=" + forward +
-          ", phoneNumber=" + phoneNumber +
-          ", service='" + service + '\'' +
-          ", country=" + country +
-          '}';
-    }
+  @Override
+  public String toString() {
+    return "SMSActivateCurrentActivation{" +
+      "id=" + id +
+      ", forward=" + forward +
+      ", phoneNumber=" + phoneNumber +
+      ", service='" + service + '\'' +
+      ", country=" + country +
+      '}';
+  }
 }
